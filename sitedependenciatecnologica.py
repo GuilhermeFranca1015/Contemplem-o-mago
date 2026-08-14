324fc7bfe7ecc145bd2045279bd416acf76e7b4e
GuilhermeFranca1015 <guilherme.franca1015@gmail.com>
2026-08-14 20:27:53 -0300

    Add HTML content for technological dependency page

---
+++ sitedependenciatecnologica.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dependência Tecnológica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>O que é dependência tecnológica?</h1>
+    <p>Dependência tecnológica é quando você depende apenas de alguma tecnológia para algo!</p>
+    <h2>lista de exemplos de dependência tecnológica:</h2>
+             <ul>
+                   <li>Quando você tem um robô que limpa a sua casa e quando você vai limpar não consegue.</li>
+                   <li>Quando você tem uma maquina de lavar louça e quando você vai lavar não consegue.</li>
+             </ul>
+     <img src="catmeme.jpg" alt="Image 1">
+     <p>Então não fique dependente das tecnológias vai fazer algo fora das telas e das tecnológias! Como ir ver o seu Pet!😺🐶🐟🐦🐹 (Se você tiver), desenhar algo🖍️ ou ir jogar bola!⚽🏀🏐</p>
+</body>
+</html>
