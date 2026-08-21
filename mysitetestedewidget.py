22cfeaaebfc42fe300ec0dd9ccbfeb5a7e3841ab
GuilhermeFranca1015 <guilherme.franca1015@gmail.com>
2026-08-21 20:15:10 -0300

    Add HTML content to mysitetestedewidget.py

---
+++ mysitetestedewidget.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dependência Tecnológica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>O que é dependência tecnológica?</h1>
+    <p>Dependência tecnológica é quando você depende apenas de alguma tecnológia para algo!</p>
+    <h2>Lista de exemplos de dependência tecnológica:</h2>
+             <ul>
+                   <li>Quando você tem um robô que limpa a sua casa e quando você vai limpar não consegue.</li>
+                   <li>Quando você tem uma maquina de lavar louça e quando você vai lavar não consegue.</li>
+             </ul>
+     <iframe src="https://www.meteoblue.com/pt/tempo/widget/daily/ip_rom%c3%aania_675514?geoloc=fixed&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&days=7&coloured=coloured&pictoicon=1&maxtemperature=1&mintemperature=1&windspeed=1&windgust=0&winddirection=1&uv=0&humidity=0&precipitation=1&precipitationprobability=1&spot=1&pressure=0&layout=light&user_key=c37d9f815e0aace3&embed_key=b3cddf78cc0237ee&sig=989a033235baadb2206493d6872a2d0e8e37e889a3ba41ea149f5585cd99436a" frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 378px;height: 349px;border: 0;overflow: hidden;"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/pt/tempo/semana/index" target="_blank" rel="noopener">meteoblue</a></div>
+     <p>Então não fique dependente das tecnológias vai fazer algo fora das telas e das tecnológias! Como ir ver o seu Pet!😺🐶🐟🐦🐹 (Se você tiver), desenhar algo🖍️ ou ir jogar bola!⚽🏀🏐</p>
+</body>
+</html>
